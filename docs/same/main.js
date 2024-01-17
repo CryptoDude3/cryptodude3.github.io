0ddd6dc7748c523ce6761c9fb62c1458e7f0503b
CryptoDude3 <66845757+CryptoDude3@users.noreply.github.com>
2024-01-16 20:45:51 -0600

    Update main.js

--- docs/same/main.js
+++ docs/same/main.js
@@ -13,6 +13,7 @@ var att = document.getElementById("att");
 var locdiv = document.getElementById("locs");
 var locinput = document.getElementById("loc");
 var extram = document.getElementById("em");
+var cl = document.getElementById("clip");
 saveb.addEventListener("click", saveToWav);
 var NWR = 1;
 var EBS = 0;
@@ -25,6 +26,7 @@ var locations = [36071];
 var par = "SENDERID";
 var play;
 var em = false;
+var clip = false;
 var tone = NWR;
 var tlen = 10;
 updateLoc();
@@ -57,8 +59,10 @@ function generateEas() {
   var l = zero_pad_int(hr.toString() + min.toString(), 4);
   tone = parseInt(att.value);
   em = extram.checked;
+  clip=cl.checked;
   create_alert(originator, event, locations, l, time, par);
   saveb.style.display = "inline-block";
+  if(cl){clipSignal(0.5);}
   addStatus("EAS Generated! Samples: " + samples.length);
   addStatus("Generated header: " + create_header_string(originator, event, locations, l, time, par));
 }
