0d8ea3446c82a6f8e837c5158e054697358eb2c0
CryptoDude3 <66845757+CryptoDude3@users.noreply.github.com>
2024-08-05 16:14:25 -0500

    Update script.js

--- docs/slope3/script.js
+++ docs/slope3/script.js
@@ -18,84 +18,92 @@ function readVec3(ptr){return {x:readFloat32(ptr),y:readFloat32(ptr+0x4),z:readF
 function setVec3(ptr,vec){if(vec.x==undefined||vec.y==undefined||vec.z==undefined){throw "Vector3 must have an x, y, and z!";}}
 function readQuat(ptr){return {x:readFloat32(ptr),y:readFloat32(ptr+0x4),z:readFloat32(ptr+0x8),w:readFloat32(ptr+0xC)};}
 function setQuat(ptr,vec){if(vec.x==undefined||vec.y==undefined||vec.z==undefined||vec.w==undefined){throw "Quaternion must have an x, y, z, and w!";}setFloat32(ptr,vec.x);setFloat32(ptr+0x4,vec.y);setFloat32(ptr+0x8,vec.z);setFloat32(ptr+0xC,vec.w);}
+window.addrs = {};
 function loadHacks(){
-window.gamemanager = 0x0114ae00;
-window.score = gamemanager+0x30;//int
+addrs.gamemanager = 0x0114ae00;
+addrs.score = gamemanager+0x30;//int
 //-----------------------------------
-window.camControl = read32(gamemanager + 0x10);//CameraController
-window.cam = read32(camControl + 0x18);//camera
-window.camDied = camControl+0x30;//bool
-window.camAllowed = camControl + 0x1c;//bool
-window.camStartPos = read32(camControl + 0x10);//Transform
+addrs.camControl = read32(gamemanager + 0x10);//CameraController
+addrs.cam = read32(camControl + 0x18);//camera
+addrs.camDied = camControl+0x30;//bool
+addrs.camAllowed = camControl + 0x1c;//bool
+addrs.camStartPos = read32(camControl + 0x10);//Transform
 //-----------------------------------
-window.ballControl = read32(gamemanager + 0xC);//BallMovementControl
-window.speedUp = ballControl + 0x34;//bool
-window.floatSpeedUpTime = ballControl + 0x3C;//float
-window.floatPushTime = ballControl + 0x38;//float
-window.firstTouchGround = ballControl+ 0x40;//bool
-window.ballDied = ballControl + 0x48;//bool
+addrs.ballControl = read32(gamemanager + 0xC);//BallMovementControl
+addrs.speedUp = ballControl + 0x34;//bool
+addrs.floatSpeedUpTime = ballControl + 0x3C;//float
+addrs.floatPushTime = ballControl + 0x38;//float
+addrs.firstTouchGround = ballControl+ 0x40;//bool
+addrs.ballDied = ballControl + 0x48;//bool
 //-----------------------------------
-window.buildingSpawner = read32(gamemanager + 0x24);//BuildingSpawnControl
+addrs.buildingSpawner = read32(gamemanager + 0x24);//BuildingSpawnControl
 //-----------------------------------
-window.movementForceControl = read32(ballControl + 0xC);//BallMovementForceControl
-window.yForce = movementForceControl + 0xC;//float
-window.zForce = movementForceControl + 0x10;//float
-window.spawnPosition = movementForceControl + 0x4C;
+addrs.movementForceControl = read32(ballControl + 0xC);//BallMovementForceControl
+addrs.yForce = movementForceControl + 0xC;//float
+addrs.zForce = movementForceControl + 0x10;//float
+addrs.spawnPosition = movementForceControl + 0x4C;
 //-----------------------------------
-window.mapLoader = read32(gamemanager+0x28);
+addrs.mapLoader = read32(gamemanager+0x28);
 //-----------------------------------
-window.gameUi = read32(gamemanager+0x2c);//GameUIControl
+addrs.gameUi = read32(gamemanager+0x2c);//GameUIControl
 //-----------------------------------
-window.playerHUD = read32(gameUi+0xC);//PlayerHUDControl
-window.hudtimer = playerHUD + 0x1c;//float
-window.hudTimeShowGuide = playerHUD + 0x18;//float
+addrs.playerHUD = read32(gameUi+0xC);//PlayerHUDControl
+addrs.hudtimer = playerHUD + 0x1c;//float
+addrs.hudTimeShowGuide = playerHUD + 0x18;//float
 //-----------------------------------
-window.mainPanel = read32(gameUi+0x10);//MainPanelControl
-window.panelAgainGame = mainPanel + 0x30;//bool
-window.panelIsPlay = mainPanel + 0x3C;//bool
-window.panelIsControl = mainPanel + 0x3D;//bool
-window.panelStartTime = mainPanel + 0x40;//float
+addrs.mainPanel = read32(gameUi+0x10);//MainPanelControl
+addrs.panelAgainGame = mainPanel + 0x30;//bool
+addrs.panelIsPlay = mainPanel + 0x3C;//bool
+addrs.panelIsControl = mainPanel + 0x3D;//bool
+addrs.panelStartTime = mainPanel + 0x40;//float
 //-----------------------------------
-window.leaderboardControl = read32(gameUi+0x14);//LeaderBoardControl
-window.leaderboardStartTime = leaderboardControl + 0x90;//float
-window.bestText = read32(leaderboardControl + 0x80);//Text
+addrs.leaderboardControl = read32(gameUi+0x14);//LeaderBoardControl
+addrs.leaderboardStartTime = leaderboardControl + 0x90;//float
+addrs.bestText = read32(leaderboardControl + 0x80);//Text
 //-----------------------------------
 }
 var mag = 100;
-document.addEventListener("keydown",(e)=>{if(e.key!=="ArrowUp"){return;}setFloat32(zForce,readFloat32(zForce)+mag)});
-document.addEventListener("keydown",(e)=>{if(e.key!=="ArrowDown"){return;}setFloat32(zForce,readFloat32(zForce)-mag)});
-document.addEventListener("keydown",(e)=>{if(e.key!=="f"){return;}set8(camAllowed,!read8(camAllowed))});
+
+
+
+let cheats = [{type:"bool",addrValue:"camAllowed",key:"F",text:"Freeze Camera"}];
+const toggles = makeDropdown("Toggles");
+cheats.forEach(cheat=>{
+switch(cheat.type){
+case "bool":
+var cheatElem = createCheat(cheat.key,cheat.text);
+toggles.addCheat(cheatElem);
+cheat.html = cheatElem;
+document.addEventListener("keydown",(e)=>{if(e.key!==cheat.key){return;}const value = read8(addrs[cheat.addrValue]);set8(value?0:1,addrs[cheat.addrValue]);cheatElem.setValue(!value);});
+break;
+}
+});
 
 
 
 
 
-
-
-
-
-
-
-function createCheat(key,text){var cheat = document.createElement("div");
+function createCheat(key,text){
+let cheat = document.createElement("div");
 cheat.className = "toggle";
 cheat.id=key.toUpperCase();
 cheat.setValue = function(value){value?this.setAttribute("enabled",""):this.removeAttribute("enabled");}
 cheat.innerHTML = `[${key}] ${text}`;
 return cheat;}
 
-
-
-function makeDropdown(cheats,text){
-var ddcontainer = document.createElement("div");
+function makeDropdown(text){
+let ddcontainer = document.createElement("div");
 ddcontainer.className = "dropdown-container";
-var arrow = document.createElement("div");
+let arrow = document.createElement("div");
 arrow.className = "dropdown-arrow";
-arrow.innerText = "\u25bc";
+arrow.innerText = "\u25ba";
 ddcontainer.appendChild(arrow);
-ddcontainer.innerText+=text;
-var content = document.createElement("div");
+let textElem = document.createElement("b");
+textElem.innerText = text;
+ddcontainer.appendChild(textElem);
+let content = document.createElement("div");
 content.className = "content";
-cheats.forEach(cheat=>content.appendChild(cheat));
+ddcontainer.addCheat = function(cheat){content.appendChild(cheat);}
 ddcontainer.addEventListener("click",()=>{
 if(content.style.display=="none" || content.style.display==""){
 arrow.innerHTML = "\u25bc";
@@ -108,3 +116,4 @@ content.style.display = "none";
 ddcontainer.appendChild(content);
 return ddcontainer;
 }
+document.querySelector(".cheat-container").appendChild(toggles);
