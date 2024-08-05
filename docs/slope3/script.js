91f697bbe8a8ded204568e684b2ce971072c34c4
CryptoDude3 <66845757+CryptoDude3@users.noreply.github.com>
2024-08-05 15:11:07 -0500

    Update script.js

--- docs/slope3/script.js
+++ docs/slope3/script.js
@@ -18,48 +18,49 @@ function readVec3(ptr){return {x:readFloat32(ptr),y:readFloat32(ptr+0x4),z:readF
 function setVec3(ptr,vec){if(vec.x==undefined||vec.y==undefined||vec.z==undefined){throw "Vector3 must have an x, y, and z!";}}
 function readQuat(ptr){return {x:readFloat32(ptr),y:readFloat32(ptr+0x4),z:readFloat32(ptr+0x8),w:readFloat32(ptr+0xC)};}
 function setQuat(ptr,vec){if(vec.x==undefined||vec.y==undefined||vec.z==undefined||vec.w==undefined){throw "Quaternion must have an x, y, z, and w!";}setFloat32(ptr,vec.x);setFloat32(ptr+0x4,vec.y);setFloat32(ptr+0x8,vec.z);setFloat32(ptr+0xC,vec.w);}
-var gamemanager = 0x0114ae00;
-var score = gamemanager+0x30;//int
+function loadHacks(){
+window.gamemanager = 0x0114ae00;
+window.score = gamemanager+0x30;//int
 //-----------------------------------
-var camControl = read32(gamemanager + 0x10);//CameraController
-var cam = read32(camControl + 0x18);//camera
-var camDied = camControl+0x30;//bool
-var camAllowed = camControl + 0x1c;//bool
-var camStartPos = read32(camControl + 0x10);//Transform
+window.camControl = read32(gamemanager + 0x10);//CameraController
+window.cam = read32(camControl + 0x18);//camera
+window.camDied = camControl+0x30;//bool
+window.camAllowed = camControl + 0x1c;//bool
+window.camStartPos = read32(camControl + 0x10);//Transform
 //-----------------------------------
-var ballControl = read32(gamemanager + 0xC);//BallMovementControl
-var speedUp = ballControl + 0x34;//bool
-var floatSpeedUpTime = ballControl + 0x3C;//float
-var floatPushTime = ballControl + 0x38;//float
-var firstTouchGround = ballControl+ 0x40;//bool
-var ballDied = ballControl + 0x48;//bool
+window.ballControl = read32(gamemanager + 0xC);//BallMovementControl
+window.speedUp = ballControl + 0x34;//bool
+window.floatSpeedUpTime = ballControl + 0x3C;//float
+window.floatPushTime = ballControl + 0x38;//float
+window.firstTouchGround = ballControl+ 0x40;//bool
+window.ballDied = ballControl + 0x48;//bool
 //-----------------------------------
-var buildingSpawner = read32(gamemanager + 0x24);//BuildingSpawnControl
+window.buildingSpawner = read32(gamemanager + 0x24);//BuildingSpawnControl
 //-----------------------------------
-var movementForceControl = read32(ballControl + 0xC);//BallMovementForceControl
-var yForce = movementForceControl + 0xC;//float
-var zForce = movementForceControl + 0x10;//float
-var spawnPosition = movementForceControl + 0x4C;
+window.movementForceControl = read32(ballControl + 0xC);//BallMovementForceControl
+window.yForce = movementForceControl + 0xC;//float
+window.zForce = movementForceControl + 0x10;//float
+window.spawnPosition = movementForceControl + 0x4C;
 //-----------------------------------
-var mapLoader = read32(gamemanager+0x28);
+window.mapLoader = read32(gamemanager+0x28);
 //-----------------------------------
-var gameUi = read32(gamemanager+0x2c);//GameUIControl
+window.gameUi = read32(gamemanager+0x2c);//GameUIControl
 //-----------------------------------
-var playerHUD = read32(gameUi+0xC);//PlayerHUDControl
-var hudtimer = playerHUD + 0x1c;//float
-var hudTimeShowGuide = playerHUD + 0x18;//float
+window.playerHUD = read32(gameUi+0xC);//PlayerHUDControl
+window.hudtimer = playerHUD + 0x1c;//float
+window.hudTimeShowGuide = playerHUD + 0x18;//float
 //-----------------------------------
-var mainPanel = read32(gameUi+0x10);//MainPanelControl
-var panelAgainGame = mainPanel + 0x30;//bool
-var panelIsPlay = mainPanel + 0x3C;//bool
-var panelIsControl = mainPanel + 0x3D;//bool
-var panelStartTime = mainPanel + 0x40;//float
+window.mainPanel = read32(gameUi+0x10);//MainPanelControl
+window.panelAgainGame = mainPanel + 0x30;//bool
+window.panelIsPlay = mainPanel + 0x3C;//bool
+window.panelIsControl = mainPanel + 0x3D;//bool
+window.panelStartTime = mainPanel + 0x40;//float
 //-----------------------------------
-var leaderboardControl = read32(gameUi+0x14);//LeaderBoardControl
-var leaderboardStartTime = leaderboardControl + 0x90;//float
-var bestText = read32(leaderboardControl + 0x80);//Text
+window.leaderboardControl = read32(gameUi+0x14);//LeaderBoardControl
+window.leaderboardStartTime = leaderboardControl + 0x90;//float
+window.bestText = read32(leaderboardControl + 0x80);//Text
 //-----------------------------------
-
+}
 var mag = 100;
 document.addEventListener("keydown",(e)=>{if(e.key!=="ArrowUp"){return;}setFloat32(zForce,readFloat32(zForce)+mag)});
 document.addEventListener("keydown",(e)=>{if(e.key!=="ArrowDown"){return;}setFloat32(zForce,readFloat32(zForce)-mag)});
